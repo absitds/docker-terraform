dcf8de47868e2318611280a1e17c1861329435e7
Abhijeet Shingate <abhijeet@exadatum.com>
2019-01-28 02:18:33 +0530

    updated docker with clean dependencies

--- src/main/init/init.sh
+++ src/main/init/init.sh
@@ -2,18 +2,24 @@
 
 #Install docker CE
 sudo apt-get update -yqq
+
 sudo apt-get install -yqq \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common \
+    maven
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+
 sudo apt-get update -yqq
+
 sudo apt-get install docker-ce -yqq
 
 #Add current user to docker group to run commands without sudo
